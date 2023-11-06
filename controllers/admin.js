8e07650d33f76e1757023d47f4d8f21363298058
san38700 <sandeepkratosj@yahoo.in>
2023-11-06 21:33:22 +0530

    query params

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
-    path: '/admin/add-product',
+    path: '/admin/add-product'
   });
 };
 
@@ -19,23 +19,16 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
+  console.log(editMode)
   if (!editMode) {
     return res.redirect('/')
   }
-  const prodId = req.params.productId
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/')
-    }
-    res.render('admin/edit-product', {
+  res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode
-  })
-  
   });
-};
-
+}
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('admin/products', {
