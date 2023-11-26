aa9f181ba3c0991c8723c00b3ed3fd996d00bde9
san38700 <sandeepkratosj@yahoo.in>
2023-11-26 07:54:09 +0530

    inserting deleting tables

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM products')
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
