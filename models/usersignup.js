597554365b5320d9c242bd1ae51985fa6ade2e8c
san38700 <sandeepkratosj@yahoo.in>
2024-06-08 15:51:22 +0530

    Deleting Cart Items

--- models/usersignup.js
+++ models/usersignup.js
@@ -1,4 +1,5 @@
-const { ObjectId } = require('mongodb')
+const { ObjectId } = require('mongodb');
+const { get } = require('../routes/admin');
 const getDb = require('../util/database').getDb
 
 class User{
@@ -41,6 +42,37 @@ class User{
         return db.collection('users')
         .updateOne({_id: new ObjectId(this._id)}, 
         {$set: {cart: updatedCart}})
+    }
+
+    getCart(){
+        const db = getDb()
+        const productIds = this.cart.items.map(i => {
+            return i.productId
+       })
+       return db
+       .collection('products')
+       .find({_id: {$in: productIds}})
+       .toArray()
+       .then(products => {
+        return products.map(p => {
+            return {
+                ...p, quantity: this.cart.items.find(i =>{
+                    return i.productId.toString() === p._id.toString()
+                }).quantity
+            }
+        })
+       })
+
+    }
+
+    deleteItemFromCart(productId){
+        const updatedCartItems = this.cart.items.filter(item => {
+            return item.productId.toString() !== productId.toString()
+        })
+        const db = getDb()
+        return db.collection('users')
+        .updateOne({_id: new ObjectId(this._id)}, 
+        {$set: {cart: {items: updatedCartItems}}})
     }
 
     static findById(userId){
